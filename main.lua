8c8e7ab7d430d4a2ce85e8da88cf18db30ab7be3
Rémy Tauziac <tauziac0remy@gmail.com>
2014-07-06 15:02:29 +0200

    State management
    Add gamestate management

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
+local Gamestate = require "hump.gamestate"
 local Camera = require "hump.camera"
 local Vector2 = require "hump.vector"
+local Timer = require "hump.timer"
 local Rectangle = require "rectangle"
 local TouchZone = require "touchZone"
 
@@ -51,9 +53,104 @@ local gameStates = {
     menu = {}
 }
 
+function gameStates.intro:init()
+    self.textString = [[Initializing... loading boot routine
+Hardware check:
+* HDD
+* RAM
+
+Input methods:
+* keyboard - OK
+* mouse - OK]]
+    self.currentTextDisplay = ""
+    Timer.addPeriodic(0.03, function()
+        self.currentTextDisplay = self.textString:sub(1, self.currentTextDisplay:len() + 1)
+    end, self.textString:len())
+    
+    Timer.add(5, function() Gamestate.switch(gameStates.menu) end )
+end
+
+function gameStates.intro:update()
+    
+end
+
+function gameStates.intro:draw()
+    love.graphics.setCanvas(mainCanvas)
+    mainCanvas:clear()
+    love.graphics.setColor(60, 255, 60, 240)
+    local mark = ""
+    if shaderElapsedTime % 1 < 0.5 then
+        mark = "_"
+    end
+    love.graphics.print(self.currentTextDisplay..mark, 15, 15)
+    love.graphics.setCanvas()
+end
+
+function gameStates.menu:update(dt)
+    local mousePosition = Vector2(love.mouse.getPosition())
+    local delta = mousePosition - previousMousePosition
+    previousMousePosition = mousePosition
+    
+    if love.mouse.isDown("l") then
+        for i, shape in ipairs(propagateLeftMouseEvent) do
+            shape:onTouchMove(mousePosition, delta * screenScale)
+        end
+    end
+end
+
+function gameStates.menu:mousepressed(x, y, button)
+    local convertedTouchLocation = (Vector2(x, y) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
+    if button == "l" then
+        local zoneHit = nil
+        for _, zone in ipairs(rootTouchZones) do
+            zoneHit = zone:touchInside(convertedTouchLocation)
+            if zoneHit then break end
+        end
+        if zoneHit then
+            zoneHit:onTouchDown(convertedTouchLocation)
+            table.insert(propagateLeftMouseEvent, zoneHit)
+        end
+    end
+end
+
+function gameStates.menu:mousereleased(x, y, button)
+    local convertedTouchLocation = (Vector2(x, y) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
+    if button == "l" then
+        for _, zone in ipairs(propagateLeftMouseEvent) do
+            zone:onTouchUp(convertedTouchLocation)
+        end
+        
+        propagateLeftMouseEvent = {}
+    end
+end
+
+function gameStates.menu:draw()
+    local convertedTouchLocation = (Vector2(love.mouse.getPosition()) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
+    
+    love.graphics.setCanvas(mainCanvas)
+    mainCanvas:clear()
+    --love.graphics.setColor(15, 30, 15, 255)
+    --love.graphics.setColor(255, 255, 255, 255)
+    --love.graphics.rectangle("fill", unpack(backgroundSize))
+    love.graphics.setColor(60, 255, 60, 240)
+    love.graphics.print("Welcome to Flag maker 2.0", 10, 10, 0, 1, 1)
+    redShape:draw()
+    yellowShape:draw()
+    
+    -- the mouse
+    love.graphics.setColor(60, 255, 60, 200)
+    love.graphics.rectangle("fill", convertedTouchLocation.x - 4, convertedTouchLocation.y, 3, 1)
+    love.graphics.rectangle("fill", convertedTouchLocation.x + 2, convertedTouchLocation.y, 3, 1)
+    love.graphics.rectangle("fill", convertedTouchLocation.x, convertedTouchLocation.y - 4, 1, 3)
+    love.graphics.rectangle("fill", convertedTouchLocation.x, convertedTouchLocation.y + 2, 1, 3)
+    love.graphics.setCanvas()
+end
+
 local computerFont = love.graphics.newFont("assets/data-latin.ttf", 12)
 
 function love.load()
+    Gamestate.registerEvents()
+    Gamestate.switch(gameStates.intro)
     modes = love.window.getFullscreenModes()
     table.sort(modes, function(a, b) return a.width * a.height < b.width * b.height end)
     love.window.setMode(modes[#modes].width, modes[#modes].height, {resizable = true, fullscreen = true, minwidth = designResolution.x, minheight = designResolution.y})
@@ -81,47 +178,18 @@ function love.resize(w, h)
 end
 
 function love.update(dt)
+    Timer.update(dt)
     shaderElapsedTime = shaderElapsedTime + dt;
     mainShader:send("elapsed_time", shaderElapsedTime)
-    
-    local mousePosition = Vector2(love.mouse.getPosition())
-    local delta = mousePosition - previousMousePosition
-    previousMousePosition = mousePosition
-    
-    if love.mouse.isDown("l") then
-        for i, shape in ipairs(propagateLeftMouseEvent) do
-            --shape.frame.origin = shape.frame.origin + (delta * screenScale)
-            shape:onTouchMove(mousePosition, delta * screenScale)
-        end
-    end
 end
 
 function love.draw()
-    local convertedTouchLocation = (Vector2(love.mouse.getPosition()) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
-    
-    love.graphics.setCanvas(mainCanvas)
-    love.graphics.setColor(15, 30, 15, 255)
-    --love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.rectangle("fill", unpack(backgroundSize))
-    love.graphics.setColor(60, 255, 60, 240)
-    love.graphics.print("Welcome to Flag maker 2.0", 10, 10, 0, 1, 1)
-    redShape:draw()
-    yellowShape:draw()
-    
-    love.graphics.rectangle("fill", convertedTouchLocation.x - 4, convertedTouchLocation.y, 3, 1)
-    love.graphics.rectangle("fill", convertedTouchLocation.x + 2, convertedTouchLocation.y, 3, 1)
-    love.graphics.rectangle("fill", convertedTouchLocation.x, convertedTouchLocation.y - 4, 1, 3)
-    love.graphics.rectangle("fill", convertedTouchLocation.x, convertedTouchLocation.y + 2, 1, 3)
-    
-    love.graphics.setCanvas()
-    
     sceneCamera:attach()
     love.graphics.setShader(mainShader)
+    love.graphics.setColor(15, 30, 15, 255)
+    love.graphics.rectangle("fill", unpack(backgroundSize))
     love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(mainCanvas)
-    
-    --draw mouse
-    love.graphics.setColor(60, 255, 60, 200)
     
     love.graphics.setShader()
     sceneCamera:detach()
@@ -138,28 +206,9 @@ function love.keypressed(key, isrepeat)
 end
 
 function love.mousepressed(x, y, button)
-    local convertedTouchLocation = (Vector2(x, y) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
-    if button == "l" then
-        local zoneHit = nil
-        for _, zone in ipairs(rootTouchZones) do
-            zoneHit = zone:touchInside(convertedTouchLocation)
-            if zoneHit then break end
-        end
-        if zoneHit then
-            zoneHit:onTouchDown(convertedTouchLocation)
-            table.insert(propagateLeftMouseEvent, zoneHit)
-        end
-    end
+    
 end
 
 function love.mousereleased(x, y, button)
-    local convertedTouchLocation = (Vector2(x, y) * screenScale) - Vector2(((love.window.getWidth() * screenScale) - designResolution.x) / 2, 0)
-    if button == "l" then
-        for _, zone in ipairs(propagateLeftMouseEvent) do
-            zone:onTouchUp(convertedTouchLocation)
-            --table.remove(propagateLeftMouseEvent)
-        end
-        
-        propagateLeftMouseEvent = {}
-    end
+    
 end
