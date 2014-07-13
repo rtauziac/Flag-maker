5194d04ec8d527c4e232e035e2add4f629cb5c98
Rémy Tauziac <tauziac0remy@gmail.com>
2014-07-14 00:01:34 +0200

    Add color switch
    Add a slider to choose color for the selected region

--- main.lua
+++ main.lua
@@ -21,11 +21,87 @@ mainCanvas:setFilter("nearest")
 local computerFont = love.graphics.newFont("assets/data-latin.ttf", 12)
 local computerFontSmall = love.graphics.newFont("assets/data-latin.ttf", 10)
 
+local selectedRegion = nil
+
 local gameStates = {
     intro = {},
     menu = {},
     createNewFlag = {}
 }
+
+function RGBtoHSV(r, g, b)
+	local h, v, s
+	local valMin, valMax, delta
+	valMin = math.min(r, math.min(g, b))
+	valMax = math.max(r, math.max(g, b))
+	v = valMax
+	delta = valMax - valMin
+	if valMax ~= 0 then
+		s = delta / valMax		-- s
+	else
+		-- r = g = b = 0		-- s = 0, v is undefined
+		return 0, v, 0
+	end
+	
+	if r == valMax then
+		h = ( g - b ) / delta		-- between yellow & magenta
+	elseif g == valMax then
+		h = 2 + ( b - r ) / delta	-- between cyan & yellow
+	else
+		h = 4 + ( r - g ) / delta	-- between magenta & cyan
+		h = h *60				-- degrees
+	end
+	
+	if h < 0 then
+		h = h + 360
+	end
+	
+	return h, s, v
+end
+
+function HSVtoRGB(h, s, v)
+	if s == 0 then -- achromatic (grey)
+		return v, v, v
+	end
+	
+	local r, g, b
+	local i
+	local f, p, q, t
+	
+	h = h / 60			-- sector 0 to 5
+	i = math.floor(h)
+	f = h - i			-- factorial part of h
+	p = v * ( 1 - s )
+	q = v * ( 1 - s * f )
+	t = v * ( 1 - s * ( 1 - f ) )
+	if i == 0 then
+		r = v
+		g = t
+		b = p
+	elseif i == 1 then
+		r = q
+		g = v
+		b = p
+	elseif i == 2 then
+		r = p
+		g = v
+		b = t
+	elseif i == 3 then
+		r = p
+		g = q
+		b = v
+	elseif i == 4 then
+		r = t
+		g = p
+		b = v
+	else
+		r = v
+		g = p
+		b = q
+	end
+	
+	return r, g, b
+end
 
 function gameStates.intro:init()
     self.textString = [[Mounting file system...
@@ -167,14 +243,53 @@ function gameStates.createNewFlag:init()
         Gamestate.switch(gameStates.menu)
     end
     
-    self.baseBackgroundFlag = TouchZone(60, 55, 200, 120)
-    self.baseBackgroundFlag.selected = false
-    self.baseBackgroundFlag.color = {255, 0, 0, 255}
-    self.baseBackgroundFlag.draw = function(self)
+	self.hueSelector = TouchZone(280, 55, 12, 120)
+	self.hueSelector.hueSelectorCanvas = love.graphics.newCanvas(self.hueSelector.frame.size.x, self.hueSelector.frame.size.y)
+	love.graphics.setCanvas(self.hueSelector.hueSelectorCanvas)
+	local row
+	for row = 0, 120 do
+		local r, g, b = HSVtoRGB(row * 3, 1, 1)
+		love.graphics.setColor(r * 255, g * 255, b * 255, 255)
+		love.graphics.rectangle("fill", 0, row, self.hueSelector.frame.size.x, row + 1)
+	end
+	love.graphics.setCanvas()
+	self.hueSelector.draw = function(self)
+		love.graphics.setColor(255, 255, 255, 255)
+		love.graphics.draw(self.hueSelectorCanvas, self.frame.origin.x, self.frame.origin.y)
+		love.graphics.setColor(255, 255, 255, 128)
+        love.graphics.rectangle("line", self.frame.origin.x + 0.5, self.frame.origin.y + 0.5, self.frame.size.x - 1, self.frame.size.y - 1)
+	end
+	self.hueSelector.onTouchUpInside = function(self, position)
+		local hue = math.max(0, math.min(359, ((position.y / self.frame.size.y) - 1) * 180))
+		local selectedRegion = self.baseRegion:getSelected()
+		if selectedRegion then
+			local r, g, b = HSVtoRGB(hue, 1, 1)
+			selectedRegion.color = {r * 255, g * 255, b * 255, 255}
+		end
+	end
+	self.hueSelector.onTouchMove = self.hueSelector.onTouchUpInside
+	
+    self.baseRegion = TouchZone(60, 55, 200, 120)
+	self.hueSelector.baseRegion = self.baseRegion
+    self.baseRegion.selected = false
+    self.baseRegion.color = {255, 0, 0, 255}
+	self.baseRegion.getSelected = function(self)
+		if self.selected == true then
+			return self
+		end
+		for _, child in ipairs(self.children) do
+			local childSelected = child:getSelected()
+			if childSelected ~= nil then
+				return childSelected
+			end
+		end
+		return nil
+	end
+    self.baseRegion.draw = function(self)
         love.graphics.setColor(unpack(self.color))
         love.graphics.rectangle("fill", self.frame.origin.x, self.frame.origin.y, self.frame.size.x, self.frame.size.y)
-        if self.selected and elapsedTime % 1.5 < 1 then
-            if elapsedTime % 1.5 < 0.5 then
+        if self.selected and elapsedTime % 1.5 < 0.5 then
+            if elapsedTime % 1.5 < 0.25 then
                 love.graphics.setColor(255, 255, 255, 64)
             else
                 love.graphics.setColor(0, 0, 0, 64)
@@ -182,12 +297,17 @@ function gameStates.createNewFlag:init()
             love.graphics.rectangle("fill", self.frame.origin.x, self.frame.origin.y, self.frame.size.x, self.frame.size.y)
         end
     end
-    self.baseBackgroundFlag.onTouchUpInside = function(self)
+    self.baseRegion.onTouchUpInside = function(self)
         self.selected = not self.selected
     end
     
     table.insert(self.rootTouchZones, self.backButton)
-    table.insert(self.rootTouchZones, self.baseBackgroundFlag)
+    table.insert(self.rootTouchZones, self.baseRegion)
+	table.insert(self.rootTouchZones, self.hueSelector)
+end
+
+function gameStates.createNewFlag:update()
+	
 end
 
 function gameStates.createNewFlag:draw()
@@ -199,7 +319,8 @@ function gameStates.createNewFlag:draw()
     love.graphics.setFont(computerFont)
     love.graphics.setColor(20, 20, 20, 240)
     self.backButton:draw()
-    self.baseBackgroundFlag:draw()
+    self.baseRegion:draw()
+	self.hueSelector:draw()
     
     -- the mouse
     love.graphics.setColor(0, 0, 0, 180)
@@ -239,10 +360,10 @@ end
 function love.load()
     Gamestate.registerEvents()
     Gamestate.switch(gameStates.menu)
-    modes = love.window.getFullscreenModes()
-    table.sort(modes, function(a, b) return a.width * a.height < b.width * b.height end)
-    love.window.setMode(modes[#modes].width, modes[#modes].height, {resizable = true, fullscreen = true, minwidth = designResolution.x, minheight = designResolution.y})
-    love.window.setTitle("Flag maker terminal")
+    --modes = love.window.getFullscreenModes()
+    --table.sort(modes, function(a, b) return a.width * a.height < b.width * b.height end)
+    love.window.setMode(designResolution.x * 2, designResolution.y * 2, {resizable = true, fullscreen = false, minwidth = designResolution.x, minheight = designResolution.y})
+    love.window.setTitle("Flag maker 2.0")
     love.mouse.setVisible(false)
     
     sceneCamera = Camera()
@@ -282,13 +403,13 @@ end
 
 function love.draw()
     sceneCamera:attach()
-    love.graphics.setShader(mainShader)
+    --love.graphics.setShader(mainShader)
     love.graphics.setColor(15, 30, 15, 255)
     love.graphics.rectangle("fill", unpack(backgroundSize))
     love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(mainCanvas)
     
-    love.graphics.setShader()
+    --love.graphics.setShader()
     sceneCamera:detach()
 end
 
